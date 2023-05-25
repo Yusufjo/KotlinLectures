25622dbd934e8ad4c07d87047e6fd7cf43831748
yusuf <yusuf.joe6161@gmail.com>
2023-05-25 10:36:55 +0300

    DersEkleme projesinde Tüm kullanıcılar girildi, ders çıkarmada Nurten ve Omer Kullanıcıları için Min size ve maks size girilecek

--- src/main/kotlin/DersEkleme.kt
+++ src/main/kotlin/DersEkleme.kt
@@ -1,7 +1,7 @@
 fun main(args: Array<String>) {
     val usernick = listOf("Yusufjoe", "NuNu12", "Beyaz.61")
     val userpass = listOf("Joefree61", "Nurten123", "Ookumus")
-    val LessonBil = setOf(
+    val LessonBil = listOf(
         "mat-1",
         "mat-2",
         "fizik-1",
@@ -12,7 +12,7 @@ fun main(args: Array<String>) {
         "data structures",
         "algorithm analysis"
     )
-    val LessonMak = setOf(
+    val LessonMak = listOf(
         "mat-1",
         "mat-2",
         "fizik-1",
@@ -25,7 +25,7 @@ fun main(args: Array<String>) {
         "termodinamik",
         "içten yanmalı motorlar"
     )
-    val LessonIng = setOf(
+    val LessonIng = listOf(
         "dilbilime giriş",
         "ingiliz kültürü",
         "çeviri uygulamaları",
@@ -56,31 +56,78 @@ fun main(args: Array<String>) {
             val AlDerslerYusuf = LessonBil.subtract(YusufVerilenDersler).subtract(YusufDersler)
             println(AlDerslerYusuf)
             println("Hangi dersleri eklemek istediğinizi yazınız:")
-            val derssecme = readln().lowercase()
-            if (YusufDersler.contains(derssecme))
+            val derssecmeY = readln().lowercase()
+            if (YusufDersler.contains(derssecmeY))
                 println("Bu dersi zaten almışsınız.")
             else {
-                val eklenendersler = derssecme.split(",")
-                YusufDersler.addAll(eklenendersler)
-                if (eklenendersler.size > 4)
+                val EklenenDerslerY = derssecmeY.split(",")
+                YusufDersler.addAll(EklenenDerslerY)
+                if (EklenenDerslerY.size > 4)
                     println("Maksimum 4 adet ders seçebilirsiniz.")
-                else if (eklenendersler.size < 2)
+                else if (EklenenDerslerY.size < 2)
                     println("Minimum 2 ders seçebilirsiniz.")
                 else
-                    if (derssecme.endsWith("2") && !YusufVerilenDersler.contains(derssecme.substring(0, 3)))
+                    if (derssecmeY.endsWith("2") && !YusufVerilenDersler.contains(derssecmeY.substring(0, 3)))
                         print("1.sini tamamlamadığınız derslerin 2.sini alamazsınız.")
-                    else print("$eklenendersler Başarılı Olarak Eklendi.\nMevcut Dersleriniz: $YusufDersler")
+                    else print("$EklenenDerslerY Başarılı Olarak Eklendi.\nMevcut Dersleriniz: $YusufDersler")
             }
-           // else if (Kull == usernick[0] && Sifre == userpass[0]
+            // else if (Kull == usernick[1] && Sifre == userpass[1]
+            println("Alınabilecek Dersler: ")
+            val AlNurtenDersler = LessonMak.subtract(NurtenVerilenDersler).subtract(NurtenDersler)
+            println(AlNurtenDersler)
+            println("Hangi Dersleri Eklemek İstediğinizi Yazınız: ")
+            val DersSecmeN = readln().lowercase()
+            if (NurtenDersler.contains(DersSecmeN))
+                println("Bu dersi zaten almışsınız.")
+            else {
+                val EklenenDerslerN = DersSecmeN.split(",")
+                NurtenDersler.addAll(EklenenDerslerN)
+                if (EklenenDerslerN.size>4)
+                    println("Minimum 4 ders seçmelisiniz.")
+                else if (EklenenDerslerN.size<2)
+                    println("Minimum 2 ders seçmelisiniz.")
+                else println("$EklenenDerslerN Başarılı olarak eklendi.\nMevcut Dersleriniz: $NurtenDersler")
+            }
+// else if (Kull == usernick[2] && Sifre == userpass[2]
+            println("Alınabilecek Dersler: ")
+            val AlOmerDersler = LessonIng.subtract(OmerVerilenDersler).subtract(OmerDersler)
+            println(AlOmerDersler)
+            println("Hangi Dersleri Eklemek İstediğinizi Yazınız: ")
+            val DersSecmeO = readln().lowercase()
+            if (OmerDersler.contains(DersSecmeN))
+                println("Bu dersi zaten almışsınız.")
+            else {
+                val EklenenDerslerO = DersSecmeN.split(",")
+                OmerDersler.addAll(EklenenDerslerO)
+                if (EklenenDerslerO.size>4)
+                    println("Minimum 4 ders seçmelisiniz.")
+                else if (EklenenDerslerO.size<2)
+                    println("Minimum 2 ders seçmelisiniz.")
+                else println("$EklenenDerslerO Başarılı olarak eklendi.\nMevcut Dersleriniz: $OmerDersler")
+            }
         }
 
 
         "2" -> {//if (Kull == usernick[0] && Sifre == userpass[0])
             println("Çıkartabileceğiniz dersler: $YusufDersler\n Hangi dersleri çıkarmak istediğinizi yazın:")
-            val inputCikar= readln().lowercase()
-            val InputCikarCok=inputCikar.split(",")
-            YusufDersler.removeAll(InputCikarCok)
+            val inputCikarY = readln().lowercase()
+            val InputCikarYCok = inputCikarY.split(",")
+            YusufDersler.removeAll(InputCikarYCok)
             println("$YusufDersler\nProgram sonlandırılıyor...")
+
+            //if (Kull == usernick[1] && Sifre == userpass[1])
+            println("Çıkartabileceğiniz dersler: $NurtenDersler\n Hangi dersleri çıkarmak istediğinizi yazın:")
+            val inputCikarN = readln().lowercase()
+            val InputCikarNCok = inputCikarN.split(",")
+            NurtenDersler.removeAll(InputCikarNCok)
+            println("$NurtenDersler\nProgram sonlandırılıyor...")
+
+            //if (Kull == usernick[2] && Sifre == userpass[2])
+            println("Çıkartabileceğiniz dersler: $NurtenDersler\n Hangi dersleri çıkarmak istediğinizi yazın:")
+            val inputCikarO = readln().lowercase()
+            val InputCikarOCok = inputCikarO.split(",")
+            OmerDersler.removeAll(InputCikarOCok)
+            println("$OmerDersler\nProgram sonlandırılıyor...")
 
         }
 
