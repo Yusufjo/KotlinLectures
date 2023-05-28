4fcfee3d3ff2b7995c0c11963dd9b964b31a4421
yusuf <yusuf.joe6161@gmail.com>
2023-05-28 12:24:20 +0300

    DersEkleme projesi tamamlandı

--- src/main/kotlin/Deneme.kt
+++ src/main/kotlin/Deneme.kt
@@ -1,38 +1,23 @@
 fun main(args: Array<String>) {
-        val questions = listOf(
-            "Başkentin adı nedir?",
-            "Dünyanın en uzun nehri hangisidir?",
-            "Python programlama dilinin yaratıcısı kimdir?"
-        )
+    //arraylist - listOf - mutablelistOf -
+   var MuList= mutableListOf(1,2,3,4)
+    println(MuList)
+    MuList[1]=10
+    println(MuList)
+    MuList.add(2,16)
+    println(MuList)
+    var listof= mutableListOf("foto-1.mp3","fjhks.mp3","ya.mp3","müzik-2","müzik-1","mat-1","mat-2")
+ var listof2= mutableListOf("resim-2","mat-1")
+    var randomBirisi=listof.random()
+    if (randomBirisi.substring(randomBirisi.length-4)!=".mp3")
+    println(randomBirisi)
+ val isim="kralsonkol"
+ println(isim.substring(isim.length-5,7))
 
-        val answers = listOf(
-            "ankara",
-            "nil",
-            "yarağım"
-        )
-
-        var index = 0
-        var chance = 2
-
-        while (index < questions.size) {
-            print("${questions[index]}: ")
-            val userInput = readLine()
-
-            if (userInput == answers[index]) {
-                println("Doğru cevap!")
-                index++
-                chance = 2 // Yanıt doğruysa şansı sıfırla
-            } else {
-                if (chance > 0) {
-                    println("Yanlış cevap! Bir şansınız daha var.")
-                    chance--
-                } else {
-                    println("Yanlış cevap! Diğer soruya geçiliyor.")
-                    index++
-                    chance = 2 // Soruya geçtiğinde şansı sıfırla
-                }
-            }
-        }
-
-        println("Tebrikler, tüm soruları doğru cevapladınız!")
+val input= "müzik-2,ya.mp3"
+ val ayir=input.split(",")
+if (!listof.contains(input.substring(0,3))&&input.endsWith("2"))
+ println("1 ini almadığınız dersin 2. sini alamazsınız.")
+ else
+  println("Başarıyla eklendi")
 }
