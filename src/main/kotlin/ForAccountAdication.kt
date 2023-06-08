07c46fe1308915ec55cf04049c7339c2bf104c5f
yusuf <yusuf.joe6161@gmail.com>
2023-06-08 04:38:14 +0300

    Döngüler üzerinde çalışmalar

--- src/main/kotlin/ForAccountAdication.kt
+++ src/main/kotlin/ForAccountAdication.kt
@@ -1,15 +1,30 @@
 fun main(args: Array<String>) {
+
     for (i in 1..Int.MAX_VALUE) {
         print("Hizmet bedeliniz yazınız:")
-        val HizmetBedeli = readln()
-        if (HizmetBedeli == "q")
-            { print("Program Sonlandırılıyor.")
+        val HizmetBedeli = readln().lowercase()
+        if (HizmetBedeli == "q") {
+            print("Program Sonlandırılıyor.")
             break
-            }
-         else {
+        } else {
             val BahsisBedeli = HizmetBedeli.toDouble() * 0.15
             val Toplam = HizmetBedeli.toDouble() + BahsisBedeli
             println("Hizmet Bedeli:$HizmetBedeli\nBahşiş Bedeli:$BahsisBedeli\nToplam Maliyet:$Toplam")
+
         }
+        val DevamOption=""
+        do {
+            println("Başka işlem yapmak ister misiniz?(y/n)")
+            val DevamOption = readln().lowercase()}
+         while (DevamOption != "y" && DevamOption != "n")
+            if (DevamOption == "n") {
+                println("Program Sonlandırılıyor...")
+                break
+            } else if (DevamOption == "y") {
+            } else {
+                println("Sadece y ya da n ile işlem sağlayabilirsiniz.")
+            }
+        }
+
     }
 }
